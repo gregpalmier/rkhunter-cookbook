b7044098cc55decd4dcaccc50bc1a284dc30b040
Greg Palmier <gregpalmier@gmail.com>
2014-08-11 09:19:22 -0500

    put apt and postfix in vagrant runlist.  fill out doc templates.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'rkhunter'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'gregpalmier'
+maintainer_email 'gregpalmier@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures rkhunter'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 depends 'apt'
 depends 'postfix'
