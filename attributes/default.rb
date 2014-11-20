684ee9abc1ce8f6a61dbd966aa4310296e6f0efd
James Andrew Cross <jms.cross@gmail.com>
2014-11-20 16:35:31 -0500

    Switched defaults to real bools and updated them to match the documentation.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
-default['rkhunter']['cron_daily_run']		= ''
-default['rkhunter']['cron_db_update']		= ''
-default['rkhunter']['db_update_email']		= 'false'
-default['rkhunter']['report_email']		= 'root'
-default['rkhunter']['apt_autogen']		= 'false'
-default['rkhunter']['nice']			= '0'
-default['rkhunter']['run_check_on_battery']	= 'false'
+default['rkhunter']['cron_daily_run']	= true
+default['rkhunter']['cron_db_update'] = true
+default['rkhunter']['db_update_email'] = false
+default['rkhunter']['report_email']	= 'root'
+default['rkhunter']['apt_autogen'] = false
+default['rkhunter']['nice']	= '0'
+default['rkhunter']['run_check_on_battery']	= false
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'gregpalmier@gmail.com'
 license 'All rights reserved'
 description 'Installs/Configures rkhunter'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.1'
+version '0.2.2'
 depends 'apt'
 depends 'postfix'
