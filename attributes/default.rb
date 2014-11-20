3819a35c07df969f21fe9aba2ffa99bbbd31542e
James Andrew Cross <jms.cross@gmail.com>
2014-11-20 16:36:14 -0500

    Switched db_update_email to true by default, as per the docs.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['rkhunter']['cron_daily_run']	= true
 default['rkhunter']['cron_db_update'] = true
-default['rkhunter']['db_update_email'] = false
+default['rkhunter']['db_update_email'] = true
 default['rkhunter']['report_email']	= 'root'
 default['rkhunter']['apt_autogen'] = false
 default['rkhunter']['nice']	= '0'
