3601c521e35d1a7d6e8110f5448e58776c91be2b
Michael Burns <michael.burns@rackspace.com>
2015-06-19 18:15:37 -0700

    Add tests

---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,25 @@
+# encoding: UTF-8
+# License:: Apache License, Version 2.0
+#
+
+require_relative 'spec_helper'
+
+describe package('rkhunter') do
+  it { should be_installed   }
+end
+
+describe file('/etc/default/rkhunter') do
+  it { should be_file }
+  it { should be_mode '644' }
+  it { should be_owned_by 'root' }
+  its(:content) { should match(/^CRON_DAILY_RUN=true/) }
+  its(:content) { should match(/^CRON_DB_UPDATE=true/) }
+  its(:content) { should match(/^DB_UPDATE_EMAIL=true/) }
+  its(:content) { should match(/^REPORT_EMAIL=you@example.com/) }
+end
+
+describe file('/etc/rkhunter.conf') do
+  it { should be_file }
+  it { should be_mode '644' }
+  it { should be_owned_by 'root' }
+end
