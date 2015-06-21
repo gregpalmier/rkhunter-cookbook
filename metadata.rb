edc0a2b2652de655613cf4cb726bb3de041653fd
Michael Burns <michael.burns@rackspace.com>
2015-06-20 21:07:20 -0700

    fix metadata license to match README, LICENSE and the recipe license header

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'rkhunter'
 maintainer 'gregpalmier'
 maintainer_email 'gregpalmier@gmail.com'
-license 'All rights reserved'
+license 'Apache 2.0'
 description 'Installs/Configures rkhunter'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.5'
