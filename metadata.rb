19542eaa1ad2407dc159e2286d7ef5594d2913b8
James Le Cuirot <james.le-cuirot@yakara.com>
2017-06-16 14:04:32 +0100

    Remove apt, postfix, yum dependencies from metadata
    The cookbook itself doesn't actually use these. They are only used in
    testing and the Berksfile already has these covered.

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,3 @@ license 'Apache 2.0'
 description 'Installs/Configures rkhunter'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.5'
-depends 'apt'
-depends 'postfix'
-depends 'yum'
