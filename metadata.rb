c2f3d66c12777d0fd4167a020d713b080a1e4e24
James Le Cuirot <james.le-cuirot@yakara.com>
2017-06-20 15:53:55 +0100

    Foodcritic

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,15 @@
 name 'rkhunter'
 maintainer 'gregpalmier'
 maintainer_email 'gregpalmier@gmail.com'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures rkhunter'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.5'
+chef_version '>= 12.0'
+
+supports 'debian'
+supports 'fedora'
+supports 'ubuntu'
+
+source_url 'https://github.com/gregpalmier/rkhunter-cookbook'
+issues_url 'https://github.com/gregpalmier/rkhunter-cookbook/issues'
